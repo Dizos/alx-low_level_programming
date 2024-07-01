4d853cd7506cabc4f3495b7f3748c2d1eeb36802
Tando Mjekula <tandomjekula@gmail.com>
2024-07-01 23:10:04 +0200

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to reverse
+ */
+void rev_string(char *s)
+{
+    int len = 0, i;
+    char temp;
+    // Find the length of the string
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    // Swap characters from the start and end of the string
+    for (i = 0; i < len / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[len - 1 - i];
+        s[len - 1 - i] = temp;
+    }
+}
