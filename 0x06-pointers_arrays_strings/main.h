c922ae2e7c89d67431f46b40bdc2580a1daa96fc
Tando Mjekula <tandomjekula@gmail.com>
2024-07-04 20:13:05 +0200

    4-rev_array.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
-
 #endif /* MAIN_H */
