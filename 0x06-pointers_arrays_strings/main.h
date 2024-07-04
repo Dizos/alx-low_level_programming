df4ee4be16b5fd08f35112ad2c1c2d7ea7a68735
Tando Mjekula <tandomjekula@gmail.com>
2024-07-04 20:58:56 +0200

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reverse_array(int *a, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-char *string_toupper(char *str);
-char *cap_string(char *str);
-char *leet(char *str);
-char *rot13(char *str);
-void print_number(int n);
+
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 int _putchar(char c);
+
 #endif /* MAIN_H */
+
