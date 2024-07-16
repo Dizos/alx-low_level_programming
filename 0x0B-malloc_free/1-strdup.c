f29f57d33367682e6046e56882503f2b766b279b
Tando Mjekula <tandomjekula@gmail.com>
2024-07-16 18:27:56 +0200

    edit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,24 @@
 #include <stdlib.h>
 #include "main.h"
 
-char *_strdup(char *str);
+char *_strdup(char *str)
 {
-	char *duplicate;
+    char *duplicate;
     unsigned int i, length = 0;
+
     if (str == NULL)
         return (NULL);
+
     while (str[length] != '\0')
         length++;
+
     duplicate = malloc((length + 1) * sizeof(char));
     if (duplicate == NULL)
         return (NULL);
+
     for (i = 0; i < length; i++)
         duplicate[i] = str[i];
     duplicate[length] = '\0';
+
     return (duplicate);
 }
