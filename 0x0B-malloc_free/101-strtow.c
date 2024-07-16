1ecf79796b93fe836929f919e6a5e049b717e963
Tando Mjekula <tandomjekula@gmail.com>
2024-07-16 22:27:18 +0200

    edit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,6 +4,7 @@
 int count_words(char *str)
 {
     int count = 0, in_word = 0;
+
     while (*str)
     {
         if (*str == ' ')
@@ -23,22 +24,28 @@ int count_words(char *str)
 char **strtow(char *str)
 {
     char **words, *word;
-    int i, j, k, len = 0, word_count, word_len;
+    int i, j, k, len = 0, word_count;
+
     if (str == NULL || *str == '\0')
         return (NULL);
+
     word_count = count_words(str);
     if (word_count == 0)
         return (NULL);
+
     words = malloc((word_count + 1) * sizeof(char *));
     if (words == NULL)
         return (NULL);
+
     for (i = 0, j = 0; i < word_count; i++)
     {
         while (str[j] == ' ')
             j++;
+
         len = 0;
         while (str[j + len] != ' ' && str[j + len] != '\0')
             len++;
+
         word = malloc((len + 1) * sizeof(char));
         if (word == NULL)
         {
@@ -47,12 +54,15 @@ char **strtow(char *str)
             free(words);
             return (NULL);
         }
+
         for (k = 0; k < len; k++)
             word[k] = str[j + k];
         word[len] = '\0';
+
         words[i] = word;
         j += len;
     }
     words[word_count] = NULL;
+
     return (words);
-}
+}       
