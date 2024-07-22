970fab372f8a2a361526916f670de225d9b999fb
Tando Mjekula <tandomjekula@gmail.com>
2024-07-22 19:18:38 +0200

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member, type = char *
+ * @age: Second member, type = float
+ * @owner: Third member, type = char *
+ * Description: Longer description
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
