7e3205ac1444ebeb093682a60ac3c26677a4285e
Tando Mjekula <tandomjekula@gmail.com>
2024-07-22 22:28:40 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,52 +2,79 @@
 #include "dog.h"
 
 /**
- * _strdup - duplicates a string
- * @str: string to duplicate
- * Return: pointer to the duplicated string or NULL if failed
+ * _strlen - returns the length of a string
+ * @str: string to measure
+ *
+ * Return: length of the string
  */
-char *_strdup(char *str)
+int _strlen(char *str)
 {
-    char *dup;
-    int len = 0, i;
-    if (str == NULL)
-        return (NULL);
+    int len = 0;
+
     while (str[len])
         len++;
-    dup = malloc(sizeof(char) * (len + 1));
-    if (dup == NULL)
-        return (NULL);
-    for (i = 0; i < len; i++)
-        dup[i] = str[i];
-    dup[len] = '\0';
-    return (dup);
+
+    return (len);
 }
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+    int i;
+
+    for (i = 0; src[i]; i++)
+        dest[i] = src[i];
+    dest[i] = '\0';
+
+    return (dest);
+}
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Return: pointer to the new dog (dog_t) or NULL if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *dog;
+    char *name_copy, *owner_copy;
+
+    if (name == NULL || owner == NULL)
+        return (NULL);
+
     dog = malloc(sizeof(dog_t));
     if (dog == NULL)
         return (NULL);
-    dog->name = _strdup(name);
-    if (dog->name == NULL)
+
+    name_copy = malloc((_strlen(name) + 1) * sizeof(char));
+    if (name_copy == NULL)
     {
         free(dog);
         return (NULL);
     }
-    dog->owner = _strdup(owner);
-    if (dog->owner == NULL)
+    _strcpy(name_copy, name);
+
+    owner_copy = malloc((_strlen(owner) + 1) * sizeof(char));
+    if (owner_copy == NULL)
     {
-        free(dog->name);
+        free(name_copy);
         free(dog);
         return (NULL);
     }
+    _strcpy(owner_copy, owner);
+
+    dog->name = name_copy;
     dog->age = age;
+    dog->owner = owner_copy;
+
     return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,10 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member, type = char *
- * @age: Second member, type = float
- * @owner: Third member, type = char *
- * Description: Longer description
+ * struct dog - Structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -14,12 +13,11 @@ struct dog
     float age;
     char *owner;
 };
+
 typedef struct dog dog_t;
 
-/* Function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
