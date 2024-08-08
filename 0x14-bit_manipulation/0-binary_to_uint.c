25c273a2b43628205ce237cc6b801e4d7c447750
Tando Mjekula <tandomjekula@gmail.com>
2024-08-08 13:53:59 +0200

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
+    unsigned int result = 0;
+    int i;
 
     if (b == NULL)
         return (0);
 
-    while (*b)
+    for (i = 0; b[i] != '\0'; i++)
     {
-        if (*b != '0' && *b != '1')
+        if (b[i] != '0' && b[i] != '1')
             return (0);
-
-        num = num * 2 + (*b - '0');
-        b++;
+        result = result * 2 + (b[i] - '0');
     }
 
-    return (num);
+    return (result);
 }
