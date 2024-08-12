07cb5d928a362213c7ab3a6882e19f6e9244bb54
Tando Mjekula <tandomjekula@gmail.com>
2024-08-12 14:57:08 +0200

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>  // For size_t
+#include <sys/types.h> /* For ssize_t */
+#include <sys/uio.h>    /* For read, write */
+#include <unistd.h>     /* For STDOUT_FILENO */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: name of the file to read.
+ * @letters: number of letters to read and print.
+ *
+ * Return: actual number of letters it could read and print,
+ * or 0 if the file can not be opened or read, filename is NULL,
+ * or write fails or does not write the expected amount of bytes.
+ */
 ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* MAIN_H */
-
