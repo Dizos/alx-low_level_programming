5fbf3dc355ec9e93be078442f31dab0e6b1adc87
Tando Mjekula <tandomjekula@gmail.com>
2024-08-12 14:52:08 +0200

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>  /* for size_t*/
-#include <sys/types.h>  /* for ssize_t*/
+#include <stddef.h>  // For size_t
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
