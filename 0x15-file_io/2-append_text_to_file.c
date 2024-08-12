43d75eb14ca31152fe11a87427f55152b1a35582
Tando Mjekula <tandomjekula@gmail.com>
2024-08-12 19:59:01 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,43 +1,40 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include "main.h" 
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: The name of the file.
- * @text_content: The NULL-terminated string to add at the end of the file.
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file.
+ * @text_content: NULL terminated string to add at the end of the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success and -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len = 0, n_written;
+    int fd;
+    ssize_t bytes_written;
 
-    if (filename == NULL)
+    if (!filename)
         return (-1);
 
-    /* Open the file in append mode */
-    fd = open(filename, O_WRONLY | O_APPEND);
+    fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
     if (fd == -1)
         return (-1);
 
-    /* If text_content is not NULL, write it to the file */
-    if (text_content != NULL)
+    if (text_content == NULL)
     {
-        while (text_content[len])
-            len++;
-
-        n_written = write(fd, text_content, len);
-        if (n_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
+        close(fd);
+        return (1);
     }
 
-    /* Close the file */
-    close(fd);
+    bytes_written = write(fd, text_content, strlen(text_content));
+    if (bytes_written == -1)
+    {
+        close(fd);
+        return (-1);
+    }
 
+    close(fd);
     return (1);
 }
-
