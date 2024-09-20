d8acd345326029262e7488407b10ae3f57ffc3e3
Tando Mjekula <tandomjekula@gmail.com>
2024-09-20 12:26:15 +0200

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,25 +3,18 @@
 /**
  * dlistint_len - Returns the number of elements in a dlistint_t list
  * @h: Pointer to the head of the list
- * Return: Number of elements in the list
+ *
+ * Return: The number of nodes in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
     size_t count = 0;
 
-    if (h == NULL)
-        return 0;
-
-    // Move to the last node
-    while (current->next != NULL)
-        current = current->next;
-
-    // Count nodes moving backwards
-    do {
+    while (h != NULL)
+    {
         count++;
-        current = current->prev;
-    } while (current != NULL);
+        h = h->next;
+    }
 
     return count;
 }
